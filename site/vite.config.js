c9f390f7377e076bc037ee6c599864fe283a133f
Suchi Prommuangkun <51702323+suchiraphanp@users.noreply.github.com>
2025-11-05 11:44:39 +0000

    add clean up

--- site/vite.config.js
+++ site/vite.config.js
@@ -1,8 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// If your repo is named "suchi-portfolio", keep this base.
-// If you later rename to username.github.io, change base to '/'.
 export default defineConfig({
   plugins: [react()],
   base: '/portfolio/',
